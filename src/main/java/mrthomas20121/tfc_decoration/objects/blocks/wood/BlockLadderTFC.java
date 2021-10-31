5f6d56234032fc06fa820fc5985b734c462d88ac
Mrthomas20121 <thomas77300@gmail.com>
2021-10-31 08:59:45 +0100

    Update BlockLadderTFC.java

--- src/main/java/mrthomas20121/tfc_decoration/objects/blocks/wood/BlockLadderTFC.java
+++ src/main/java/mrthomas20121/tfc_decoration/objects/blocks/wood/BlockLadderTFC.java
@@ -3,6 +3,7 @@ package mrthomas20121.tfc_decoration.objects.blocks.wood;
 import mrthomas20121.tfc_decoration.api.ModTypes;
 import net.dries007.tfc.api.types.Tree;
 import net.minecraft.block.BlockLadder;
+import net.minecraft.block.SoundType;
 
 public class BlockLadderTFC extends BlockLadder {
 
@@ -11,5 +12,6 @@ public class BlockLadderTFC extends BlockLadder {
     public BlockLadderTFC(Tree tree, ModTypes.WoodType type) {
         this.wood = tree;
         ModTypes.addWood(tree, type, this);
+        this.setSoundType(SoundType.LADDER);
     }
 }
