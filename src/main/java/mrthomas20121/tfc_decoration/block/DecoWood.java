220830511dcd35b70625c079861e7043270dd872
Mrthomas20121 <thomas77300@gmail.com>
2023-11-17 16:17:50 +0100

    1.20 update

--- src/main/java/mrthomas20121/tfc_decoration/block/DecoWood.java
+++ src/main/java/mrthomas20121/tfc_decoration/block/DecoWood.java
@@ -1,45 +1,45 @@
 package mrthomas20121.tfc_decoration.block;
 
-import net.minecraft.world.level.material.MaterialColor;
+import net.minecraft.world.level.material.MapColor;
 
 public enum DecoWood {
 
-    black(MaterialColor.COLOR_BLACK),
-    blue(MaterialColor.COLOR_BLUE),
-    cyan(MaterialColor.COLOR_CYAN),
-    gray(MaterialColor.COLOR_GRAY),
-    green(MaterialColor.COLOR_GREEN),
-    light_blue(MaterialColor.COLOR_LIGHT_BLUE),
-    light_gray(MaterialColor.COLOR_LIGHT_GRAY),
-    lime(MaterialColor.COLOR_LIGHT_GREEN),
-    magenta(MaterialColor.COLOR_MAGENTA),
-    orange(MaterialColor.COLOR_ORANGE),
-    pink(MaterialColor.COLOR_PINK),
-    purple(MaterialColor.COLOR_PURPLE),
-    white(MaterialColor.QUARTZ),
-    yellow(MaterialColor.COLOR_YELLOW),
-    black_terracotta(MaterialColor.TERRACOTTA_BLACK),
-    blue_terracotta(MaterialColor.TERRACOTTA_BLUE),
-    cyan_terracotta(MaterialColor.TERRACOTTA_CYAN),
-    gray_terracotta(MaterialColor.TERRACOTTA_GRAY),
-    green_terracotta(MaterialColor.TERRACOTTA_GREEN),
-    light_blue_terracotta(MaterialColor.TERRACOTTA_LIGHT_BLUE),
-    light_gray_terracotta(MaterialColor.TERRACOTTA_LIGHT_GRAY),
-    lime_terracotta(MaterialColor.TERRACOTTA_LIGHT_GREEN),
-    magenta_terracotta(MaterialColor.TERRACOTTA_MAGENTA),
-    orange_terracotta(MaterialColor.TERRACOTTA_ORANGE),
-    pink_terracotta(MaterialColor.TERRACOTTA_PINK),
-    purple_terracotta(MaterialColor.TERRACOTTA_PURPLE),
-    white_terracotta(MaterialColor.TERRACOTTA_WHITE),
-    yellow_terracotta(MaterialColor.TERRACOTTA_YELLOW);
+    black(MapColor.COLOR_BLACK),
+    blue(MapColor.COLOR_BLUE),
+    cyan(MapColor.COLOR_CYAN),
+    gray(MapColor.COLOR_GRAY),
+    green(MapColor.COLOR_GREEN),
+    light_blue(MapColor.COLOR_LIGHT_BLUE),
+    light_gray(MapColor.COLOR_LIGHT_GRAY),
+    lime(MapColor.COLOR_LIGHT_GREEN),
+    magenta(MapColor.COLOR_MAGENTA),
+    orange(MapColor.COLOR_ORANGE),
+    pink(MapColor.COLOR_PINK),
+    purple(MapColor.COLOR_PURPLE),
+    white(MapColor.QUARTZ),
+    yellow(MapColor.COLOR_YELLOW),
+    black_terracotta(MapColor.TERRACOTTA_BLACK),
+    blue_terracotta(MapColor.TERRACOTTA_BLUE),
+    cyan_terracotta(MapColor.TERRACOTTA_CYAN),
+    gray_terracotta(MapColor.TERRACOTTA_GRAY),
+    green_terracotta(MapColor.TERRACOTTA_GREEN),
+    light_blue_terracotta(MapColor.TERRACOTTA_LIGHT_BLUE),
+    light_gray_terracotta(MapColor.TERRACOTTA_LIGHT_GRAY),
+    lime_terracotta(MapColor.TERRACOTTA_LIGHT_GREEN),
+    magenta_terracotta(MapColor.TERRACOTTA_MAGENTA),
+    orange_terracotta(MapColor.TERRACOTTA_ORANGE),
+    pink_terracotta(MapColor.TERRACOTTA_PINK),
+    purple_terracotta(MapColor.TERRACOTTA_PURPLE),
+    white_terracotta(MapColor.TERRACOTTA_WHITE),
+    yellow_terracotta(MapColor.TERRACOTTA_YELLOW);
 
-    private final MaterialColor color;
+    private final MapColor color;
 
-    DecoWood(MaterialColor color) {
+    DecoWood(MapColor color) {
         this.color = color;
     }
 
-    public MaterialColor getColor() {
+    public MapColor getColor() {
         return color;
     }
 }
--- src/main/java/mrthomas20121/tfc_decoration/block/TFCDecBlocks.java
+++ src/main/java/mrthomas20121/tfc_decoration/block/TFCDecBlocks.java
@@ -2,7 +2,6 @@ package mrthomas20121.tfc_decoration.block;
 
 import mrthomas20121.tfc_decoration.TFCDecoration;
 import mrthomas20121.tfc_decoration.item.TFCDecItems;
-import net.dries007.tfc.common.TFCItemGroup;
 import net.dries007.tfc.common.blocks.DecorationBlockRegistryObject;
 import net.dries007.tfc.common.blocks.ExtendedBlock;
 import net.dries007.tfc.common.blocks.ExtendedProperties;
@@ -14,7 +13,7 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.material.MapColor;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -29,39 +28,31 @@ public class TFCDecBlocks {
 
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, TFCDecoration.mod_id);
 
-    public static final Map<Rock, RegistryObject<RotatedPillarBlock>> PILLARS = Helpers.mapOfKeys(Rock.class, rock -> register("pillar/" + rock.name(), () -> new RotatedPillarBlock(BlockBehaviour.Properties.of(Material.STONE).strength(2f))));
+    public static final Map<Rock, RegistryObject<RotatedPillarBlock>> PILLARS = Helpers.mapOfKeys(Rock.class, rock -> register("pillar/" + rock.name(), () -> new RotatedPillarBlock(BlockBehaviour.Properties.of().mapColor(rock.color()).strength(2f))));
 
-    public static final Map<Rock, RegistryObject<Block>> ROCKWOOL = Helpers.mapOfKeys(Rock.class, rock -> register("rockwool/" + rock.name(), () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(2f))));
+    public static final Map<Rock, RegistryObject<Block>> ROCKWOOL = Helpers.mapOfKeys(Rock.class, rock -> register("rockwool/" + rock.name(), () -> new Block(BlockBehaviour.Properties.of().mapColor(rock.color()).strength(2f))));
 
     public static final Map<Rock, DecorationBlockRegistryObject> ROCKWOOL_DECORATIONS = Helpers.mapOfKeys(Rock.class, rock -> new DecorationBlockRegistryObject(
-            register(("rockwool/" + rock.name() + "_slab"), () -> new SlabBlock(BlockBehaviour.Properties.of(Material.STONE).strength(2f))),
-            register(("rockwool/" + rock.name() + "_stairs"), () -> new StairBlock(() -> ROCKWOOL.get(rock).get().defaultBlockState(), BlockBehaviour.Properties.of(Material.STONE).strength(2f))),
-            register(("rockwool/" + rock.name() + "_wall"), () -> new WallBlock(BlockBehaviour.Properties.of(Material.STONE).strength(2f)))
+            register(("rockwool/" + rock.name() + "_slab"), () -> new SlabBlock(BlockBehaviour.Properties.of().mapColor(MapColor.STONE).sound(SoundType.STONE).strength(2f))),
+            register(("rockwool/" + rock.name() + "_stairs"), () -> new StairBlock(() -> ROCKWOOL.get(rock).get().defaultBlockState(), BlockBehaviour.Properties.of().mapColor(MapColor.STONE).sound(SoundType.STONE).strength(2f))),
+            register(("rockwool/" + rock.name() + "_wall"), () -> new WallBlock(BlockBehaviour.Properties.of().mapColor(MapColor.STONE).sound(SoundType.STONE).strength(2f)))
     ));
 
-    public static RegistryObject<Block> POLISHED_FIRE_CLAY = register("polished_fire_clay", 2f);
-
-    public static final DecorationBlockRegistryObject POLISHED_FIRE_CLAY_DECORATIONS = new DecorationBlockRegistryObject(
-            register(("polished_fire_clay_slab"), () -> new SlabBlock(BlockBehaviour.Properties.of(Material.STONE).strength(2f))),
-            register(("polished_fire_clay_stairs"), () -> new StairBlock(() -> POLISHED_FIRE_CLAY.get().defaultBlockState(), BlockBehaviour.Properties.of(Material.STONE).strength(2f))),
-            register(("polished_fire_clay_wall"), () -> new WallBlock(BlockBehaviour.Properties.of(Material.STONE).strength(2f)))
-    );
-
-    public static final Map<DecoWood, RegistryObject<Block>> WOOD_PLANKS = Helpers.mapOfKeys(DecoWood.class, decoWood -> register("wood/%s_planks".formatted(decoWood), () -> new ExtendedBlock(ExtendedProperties.of(Material.WOOD, decoWood.getColor()).sound(SoundType.WOOD).flammableLikePlanks().strength(1.5f, 3.0f))));
+    public static final Map<DecoWood, RegistryObject<Block>> WOOD_PLANKS = Helpers.mapOfKeys(DecoWood.class, decoWood -> register("wood/%s_planks".formatted(decoWood), () -> new ExtendedBlock(ExtendedProperties.of().mapColor(decoWood.getColor()).sound(SoundType.WOOD).flammableLikePlanks().strength(1.5f, 3.0f))));
 
     public static final Map<DecoWood, WoodBlockRegistryObject> WOOD_DECORATIONS = Helpers.mapOfKeys(DecoWood.class, wood -> new WoodBlockRegistryObject(
-            register(("wood/" + wood.name() + "_slab"), () -> new TFCSlabBlock(ExtendedProperties.of(Material.WOOD).flammableLikePlanks().sound(SoundType.WOOD).strength(1.5f, 3.0f))),
-            register(("wood/" + wood.name() + "_stairs"), () -> new TFCStairBlock(() -> WOOD_PLANKS.get(wood).get().defaultBlockState(), ExtendedProperties.of(Material.WOOD).sound(SoundType.WOOD).flammableLikePlanks().strength(1.5f, 3.0f)))
+            register(("wood/" + wood.name() + "_slab"), () -> new TFCSlabBlock(ExtendedProperties.of().mapColor(wood.getColor()).flammableLikePlanks().sound(SoundType.WOOD).strength(1.5f, 3.0f))),
+            register(("wood/" + wood.name() + "_stairs"), () -> new TFCStairBlock(() -> WOOD_PLANKS.get(wood).get().defaultBlockState(), ExtendedProperties.of().mapColor(wood.getColor()).sound(SoundType.WOOD).flammableLikePlanks().strength(1.5f, 3.0f)))
     ));
 
     private static RegistryObject<Block> register(String name, float strength)
     {
-        return register(name, () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(strength)), block -> new BlockItem(block, new Item.Properties().tab(TFCItemGroup.DECORATIONS)));
+        return register(name, () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.STONE).strength(strength)),block -> new BlockItem(block, new Item.Properties()));
     }
 
     private static <T extends Block> RegistryObject<T> register(String name, Supplier<T> blockSupplier)
     {
-        return register(name, blockSupplier, block -> new BlockItem(block, new Item.Properties().tab(TFCItemGroup.DECORATIONS)));
+        return register(name, blockSupplier, block -> new BlockItem(block, new Item.Properties()));
     }
 
     private static <T extends Block> RegistryObject<T> register(String name, Supplier<T> blockSupplier, Item.Properties blockItemProperties)
